0328eb486a8c65b20fa9bb548e1867131149b42a
yurison <yu_ri_son@naver.com>
2023-08-23 07:57:14 +0900

    Exception

---
+++ backend/src/main/java/backend/global/exception/BadRequestException.java
@@ -0,0 +1,16 @@
+package backend.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BadRequestException extends RuntimeException {
+
+    private final int code;
+    private final String message;
+
+    public BadRequestException(final ExceptionCode exceptionCode) {
+        this.code = exceptionCode.getCode();
+        this.message = exceptionCode.getMessage();
+    }
+
+}
