0abcafd92c71b681ffab9d9f81fb0528d2e4117a
yurison <yu_ri_son@naver.com>
2023-08-23 02:09:04 +0900

    Member 관련 기초적인 코드 구현

---
+++ backend/src/main/java/backend/domain/member/Member.java
@@ -0,0 +1,33 @@
+package backend.domain.member;
+
+import backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+}
---
+++ backend/src/main/java/backend/service/MemberService.java
@@ -0,0 +1,13 @@
+package backend.service;
+
+import backend.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+}
