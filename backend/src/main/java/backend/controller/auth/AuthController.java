5593df332cefec8051f9015492931a4730941174
yurison <yu_ri_son@naver.com>
2023-08-24 19:26:02 +0900

    이메일 로그인 구현

--- backend/src/main/java/backend/controller/auth/AuthController.java
+++ backend/src/main/java/backend/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
-package backend.controller;
+package backend.controller.auth;
 
+import backend.domain.member.Member;
 import backend.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,27 +8,43 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/auth")
 public class AuthController {
 
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginByEmail(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        Member member = memberService.loginByEmail(username, passwordEncoder.encode(password));
+
+        LoginResponse loginResponse = new LoginResponse().toMember(member);
+
+        return ResponseEntity.ok(loginResponse);
+    }
+
     @GetMapping("/login/oauth2/code/{provider}")
-    public ResponseEntity<LoginResponse> loginByOAuth2(@PathVariable String provider, Authentication authentication) {
+    public ResponseEntity<LoginResponse> loginByOAuth2(
+            @PathVariable String provider,
+            Authentication authentication
+    ) {
+
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
         DefaultOAuth2User oAuth2User = (DefaultOAuth2User) oauthToken.getPrincipal();
 
-        String name = oAuth2User.getName();
+        String username = oAuth2User.getName();
 
+        Member member = memberService.loginBySocial(provider, username);
 
+        LoginResponse loginResponse = new LoginResponse().toMember(member);
+
+        return ResponseEntity.ok(loginResponse);
     }
 
 }
