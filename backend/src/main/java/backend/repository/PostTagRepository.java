079ff124573773e465047fdb96ec56d7114215d5
yurison <yu_ri_son@naver.com>
2023-09-01 22:38:34 +0900

    Swagger 추가

--- backend/src/main/java/backend/repository/PostTagRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.domain.postTag.PostTag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostTagRepository extends JpaRepository<PostTag, Long> {
-}
