de3a712b34610d4bff6e5d5945cc9af5791c7b7b
yurison <yu_ri_son@naver.com>
2023-09-08 23:16:32 +0900

    Comment CRUD 구현

--- backend/src/main/java/backend/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/backend/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package backend.domain.comment.repository;
 
 import backend.domain.comment.Comment;
+import backend.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPost(Post post);
 }
--- backend/src/main/java/backend/global/exception/ExceptionCode.java
+++ backend/src/main/java/backend/global/exception/ExceptionCode.java
@@ -7,13 +7,15 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionCode {
 
-    UNAUTHORIZED(0000, "접근 권한이 없습니다."),
+    UNAUTHORIZED(0000, "권한이 없습니다."),
 
     MEMBER_NOT_FOUND(1000, "존재하지 않는 회원입니다."),
     MEMBER_USERNAME_ALREADY_EXISTS(1001, "이미 존재하는 회원입니다."),
     MEMBER_PASSWORD_DO_NOT_MATCH(1002, "비밀번호가 일치하지 않습니다."),
 
     POST_NOT_FOUND(2000, "존재하지 않는 글입니다."),
+
+    COMMENT_NOT_FOUND(3000, "존재하지 않는 댓글입니다."),
 
     CATEGORY_NOT_FOUND(4000, "존재하지 않는 카테고리입니다."),
     
