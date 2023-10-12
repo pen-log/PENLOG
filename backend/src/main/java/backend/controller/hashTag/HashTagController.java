aef298c93cb20e59a5ab6e77391e147d8252456b
yurison <yu_ri_son@naver.com>
2023-10-12 23:43:22 +0900

    HashTagController 수정

--- backend/src/main/java/backend/controller/hashTag/HashTagController.java
+++ backend/src/main/java/backend/controller/hashTag/HashTagController.java
@@ -1,7 +1,7 @@
 package backend.controller.hashTag;
 
 import backend.domain.hashTag.HashTag;
-import backend.domain.hashTag.dto.HashTagCreateRequest;
+import backend.domain.hashTag.dto.HashTagRequest;
 import backend.domain.hashTag.service.HashTagService;
 import backend.domain.post.Post;
 import backend.domain.post.service.PostService;
@@ -23,23 +23,23 @@ public class HashTagController {
     private final HashTagService hashTagService;
     private final PostService postService;
 
-    @GetMapping("/{postId}")
     @Operation(summary = "특정 글의 해시태그 조회")
+    @GetMapping("/{postId}")
     public List<HashTag> getHashTags(@PathVariable Long postId) {
         Post post = postService.findById(postId);
 
         return post.getHashTags();
     }
 
-    @PostMapping("/create")
     @Operation(summary = "해시태그 생성")
-    public HashTag create(@Valid @RequestBody HashTagCreateRequest request) {
+    @PostMapping("/create")
+    public HashTag create(@Valid @RequestBody HashTagRequest request) {
         return hashTagService.create(request);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
     @Operation(summary = "해시태그 삭제")
+//    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         hashTagService.deleteById(id);
     }
--- backend/src/main/java/backend/domain/hashTag/dto/HashTagRequest.java
+++ backend/src/main/java/backend/domain/hashTag/dto/HashTagRequest.java
@@ -3,10 +3,12 @@ package backend.domain.hashTag.dto;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
-public class HashTagCreateRequest {
+public class HashTagRequest {
 
     @Size(min = 2, message = "2자 이상 입력해주세요.")
     private String name;
